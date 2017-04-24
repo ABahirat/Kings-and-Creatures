18d93883a155b0dd253248653540332eca5851e3
Peter McPadden <vindication1118@gmail.com>
2017-04-24 15:30:59 -0600

    Added Button
    Adds button that changes text.

--- SwingProject/src/main/java/kingsAndCreatures.java
+++ SwingProject/src/main/java/kingsAndCreatures.java
@@ -2,28 +2,50 @@
  * Created by Peter on 4/24/2017.
  */
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 import model.character.character;
 
 
 public class kingsAndCreatures {
+    static String name;
 
+    public static void doButtonstuff(JLabel label){
+        label.setText("I am now a changed string");
+        //System.out.println(someString);
+    }
 
     public static void main(String args[]){
         String mystring = "I am a string";
         JFrame mainFrame = new JFrame("Kings and Creatures");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setSize(512, 512);
-        mainFrame.setVisible(true);
+
         JPanel panel = new JPanel();
 
+
+
         character joe = new character("Joe", "Knight");
-        String name = joe.getName();
-        JLabel joename = new JLabel(name);
+        name = joe.getName();
+        final JLabel joename = new JLabel(name);
         joename.setText(name);
         panel.add(joename);
+        // create our jbutton
+        JButton showDialogButton = new JButton("Click Me");
+
+        // add the listener to the jbutton to handle the "pressed" event
+        showDialogButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                doButtonstuff(joename);
+            }
+        });
+        panel.add(showDialogButton);
         joename.setVisible(true);
         mainFrame.add(panel);
+        mainFrame.setVisible(true);
 
 
     }
