302a7bf93fb78b7f0b6e90d3e09a0dc692405848
ABahirat <amba9583@colorado.edu>
2017-04-26 11:11:53 -0600

    Added functions inside DM and Session classes

--- SwingProject/src/main/java/model/character/dungeonMaster.java
+++ SwingProject/src/main/java/model/character/dungeonMaster.java
@@ -1,7 +1,20 @@
 package model.character;
-
+import java.util.*;
 /**
  * Created by vindi on 4/25/2017.
  */
 public class dungeonMaster {
+    public ArrayList<session> sessionList;
+    public session activeSession;
+
+    public session createSession() {
+        session new_Session = new session();
+
+        return new_Session;
+    }
+
+    public void addPlayer(session Session, Player Player){
+        Session.addPlayer(Player);
+    }
+
 }
--- SwingProject/src/main/java/model/character/session.java
+++ SwingProject/src/main/java/model/character/session.java
@@ -1,10 +1,49 @@
 package model.character;
 import model.character.Player;
 import model.character.character;
+import java.util.*;
 /**
  * Created by Peter on 4/25/2017.
  */
 public class session {
+    String name;
+    String password;
+    ArrayList<Player> playerList;
+    ArrayList<room> map;
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPlayerList(ArrayList<Player> playerList) {
+        this.playerList = playerList;
+    }
+
+    public void setMap(ArrayList<room> map) {
+        this.map = map;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public ArrayList<Player> getPlayerList() {
+        return playerList;
+    }
+
+    public ArrayList<room> getMap() {
+        return map;
+    }
+
+    public void addPlayer(Player Player){
+        playerList.add(Player);
+    }
 }
