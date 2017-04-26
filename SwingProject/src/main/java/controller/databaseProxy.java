8e07a22373f413f06855f4cbc0536a7c686b11ae
ABahirat <amba9583@colorado.edu>
2017-04-26 11:48:54 -0600

    Added controller package, broke everything hibernate related..

---
+++ SwingProject/src/main/java/controller/databaseProxy.java
@@ -0,0 +1,20 @@
+package controller;
+
+import model.character.character;
+import model.character.dungeonMaster;
+import model.character.Player;
+import java.util.*;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Created by Ameya on 4/26/17.
+ */
+public class databaseProxy {
+
+
+}
